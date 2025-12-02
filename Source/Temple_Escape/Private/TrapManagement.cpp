84e431644790b1e5eb4d61aa110582e77abb58ff
unknown <mlabady@gamingcampus.local>
2025-12-02 19:05:03 +0100

    Creation de BP_BaseTrap (dérive de BaseTrap.h), blueprint parent pour les blueprint des pièges. Creation de BP_TrapManagement (dérive de TrapMangement.h), gère la creation/destruction/modification des pièges pièges BP_BallTrap : une grosse boule qui suit le joueur et l'ecrase pieges BP_SpikesTrap : zone où des piques sortent du sol quand le joueur marche dedans Composant FollowComponent : deplace/dirige un objet vers un autre Composant HealthCompnent : pour l'affichage de la santé et sa modification Composant DamageComponent : responsable d'infliger des dégats, il sera greffer aux pièges Interface TrapActivable : active/deastive les pièges
    A faire : réfléchir à comment implementer l'architecture de façon SOLID en évitant les hards references,
    utiliser les interfaces et Event Dispatcher, et surtout implémenter les pieges et tout le reste

---
+++ Source/Temple_Escape/Private/TrapManagement.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TrapManagement.h"
+
+// Sets default values
+ATrapManagement::ATrapManagement()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ATrapManagement::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATrapManagement::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Temple_Escape/Public/TrapManagement.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BaseTrap.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TrapManagement.generated.h"
+
+
+//class BaseTrap; //forward declaration
+
+UCLASS(Blueprintable) //pour afficher la classe dans l’éditeur unreal
+class TEMPLE_ESCAPE_API ATrapManagement : public AActor
+{
+	GENERATED_BODY()
+
+public : 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Traps", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<ABaseTrap> trapClass;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Traps", meta = (AllowPrivateAccess = "true"))
+	//TArray<ABaseTrap*> ActiveTraps;
+
+
+	
+public:	
+	// Sets default values for this actor's properties
+	ATrapManagement();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
